d909dfdd785da5409ac5bc8255c8e84022a4fd5e
Evan Zuo <739221432@qq.com>
2023-06-26 21:00:46 +0800

    add echarts

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,82 @@
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png" />
+    12
     <view class="text-area">
-      <text class="title">{{ title }}</text>
+          <canvas canvas-id="column" id="column" class="charts" @touchend="tap"
+          />
     </view>
   </view>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import uCharts from '@qiun/ucharts';
+import { onMounted, ref } from 'vue'
 const title = ref('Hello')
+
+const uChartsInstance = {};
+const cWidth = 750;
+const cHeight = 500;
+
+const getServerData = () => {
+      //模拟从服务器获取数据时的延时
+      setTimeout(() => {
+        //模拟服务器返回数据，如果数据格式和标准格式不同，需自行按下面的格式拼接
+        let res = {
+            categories: ["2016","2017","2018","2019","2020","2021"],
+            series: [
+              {
+                name: "目标值",
+                data: [35,36,31,33,13,34]
+              },
+              {
+                name: "完成量",
+                data: [18,27,21,24,6,28]
+              }
+            ]
+          };
+        drawCharts('column', res);
+      }, 500);
+    }
+    
+    const drawCharts = (id, data) => {
+      const ctx = uni.createCanvasContext(id);
+      console.log(uCharts, id, data)
+      uChartsInstance[id] = new uCharts({
+        type: "column",
+        context: ctx,
+        width: cWidth,
+        height: cHeight,
+        categories: data.categories,
+        series: data.series,
+        animation: true,
+        background: "#FFFFFF",
+        padding: [15,15,0,5],
+        xAxis: {
+          disableGrid: true
+        },
+        yAxis: {
+          data: [{min: 0}]
+        },
+        extra: {
+          column: {
+            type: "group"
+          }
+        }
+      });
+    }
+    const tap = (e) => {
+      uChartsInstance[e.target.id].touchLegend(e);
+      uChartsInstance[e.target.id].showToolTip(e);
+    }
+
+onMounted(() => {
+    getServerData();
+})
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+<style scoped>
+  .charts{
+    width: 750rpx;
+    height: 500rpx;
+  }
 </style>
---
+++ src/s.d.ts
@@ -0,0 +1,3 @@
+declare module '@qiun/ucharts' {
+
+};
