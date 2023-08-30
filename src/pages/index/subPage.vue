5486d080997a20738b59535150dc518e4eeec76f
Evan Zuo <739221432@qq.com>
2023-08-30 16:03:20 +0800

    feat：page

---
+++ src/pages/index/subPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <view class='flipper' style="">
+        <view class='front'>
+            <text class='add-journalBook-text'>新建手帐本</text>
+        </view>
+        <view class='back'>
+            <view class='input-title'>手帐本名称：</view>
+        </view>
+    </view>
+</template>
+<script lang="ts" setup>
+</script>
+<style lang="">
+
+</style>
